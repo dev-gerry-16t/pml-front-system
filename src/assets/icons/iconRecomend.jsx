a2bd196fee96f8a5e64762b3852260d24c3d789a
dev-gerry-16t <69758055+dev-gerry-16t@users.noreply.github.com>
2022-08-22 14:02:38 -0500

    save icons svg

--- src/assets/icons/iconRecomend.jsx
+++ src/assets/icons/iconRecomend.jsx
@@ -3,7 +3,148 @@ import { ReactComponent as Document } from "../svg/iconRecomend.svg";
 
 const IconRecomend = (props) => {
   const { fill = "none", color = "#000000", size = "24" } = props;
-  return <Document stroke={color} width={size} height={size} />;
+  return (
+    <svg
+      width="41"
+      height="47"
+      viewBox="0 0 41 47"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path d="M2 8.14258V38.8422V8.14258Z" fill="white" />
+      <path
+        d="M2 8.14258V38.8422"
+        stroke={color}
+        strokeWidth="3"
+        strokeLinecap="round"
+      />
+      <path d="M38.8594 17.3574V38.8574V17.3574Z" fill="white" />
+      <path
+        d="M38.8594 17.3574V38.8574"
+        stroke={color}
+        strokeWidth="3"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+      <path d="M8.14453 2H23.5017H8.14453Z" fill="white" />
+      <path
+        d="M8.14453 2H23.5017"
+        stroke={color}
+        strokeWidth="3"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+      <path d="M8.14453 45H32.716H8.14453Z" fill="white" />
+      <path
+        d="M8.14453 45H32.716"
+        stroke={color}
+        strokeWidth="3"
+        strokeLinecap="round"
+      />
+      <path
+        d="M38.8577 38.8574C38.8745 41.9253 35.7863 45.0003 32.7148 45.0003L38.8577 38.8574Z"
+        fill="white"
+      />
+      <path
+        d="M38.8577 38.8574C38.8745 41.9253 35.7863 45.0003 32.7148 45.0003"
+        stroke={color}
+        strokeWidth="3"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+      <path
+        d="M2.00168 38.8574C2.00168 41.9288 5.0731 45.0003 8.14453 45.0003L2.00168 38.8574Z"
+        fill="white"
+      />
+      <path
+        d="M2.00168 38.8574C2.00168 41.9288 5.0731 45.0003 8.14453 45.0003"
+        stroke={color}
+        strokeWidth="3"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+      <path
+        d="M2.00168 8.13867C2.00168 5.0717 5.0731 2.06867 8.14453 2.00027L2.00168 8.13867Z"
+        fill="white"
+      />
+      <path
+        d="M2.00168 8.13867C2.00168 5.0717 5.0731 2.06867 8.14453 2.00027"
+        stroke={color}
+        strokeWidth="3"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+      <path d="M38.8404 17.3709L23.5 2L38.8404 17.3709Z" fill="white" />
+      <path
+        d="M38.8404 17.3709L23.5 2"
+        stroke={color}
+        strokeWidth="3"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+      <path
+        d="M23.5017 11.2139C23.5111 14.2686 26.5855 17.3567 29.6445 17.3567L23.5017 11.2139Z"
+        fill="white"
+      />
+      <path
+        d="M23.5017 11.2139C23.5111 14.2686 26.5855 17.3567 29.6445 17.3567"
+        stroke={color}
+        strokeWidth="3"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+      <path d="M23.5 11.2143V2V11.2143Z" fill="white" />
+      <path
+        d="M23.5 11.2143V2"
+        stroke={color}
+        strokeWidth="3"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+      <path d="M29.6445 17.3574H38.8588H29.6445Z" fill="white" />
+      <path
+        d="M29.6445 17.3574H38.8588"
+        stroke={color}
+        strokeWidth="3"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+      <path d="M8.14453 38.8574H17.3588H8.14453Z" fill="white" />
+      <path
+        d="M8.14453 38.8574H17.3588"
+        stroke={color}
+        strokeWidth="3"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+      <path d="M8.14453 32.7139H23.5017H8.14453Z" fill="white" />
+      <path
+        d="M8.14453 32.7139H23.5017"
+        stroke={color}
+        strokeWidth="3"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+      <path d="M8.14453 26.5723H17.3588H8.14453Z" fill="white" />
+      <path
+        d="M8.14453 26.5723H17.3588"
+        stroke={color}
+        strokeWidth="3"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+      <path
+        d="M31 33L32.5933 36.0003L36 36.4143L33.5 38.6667L34 42L31 40.1247L28 42L28.5 38.6667L26 36.4143L29.5 36.0003L31 33Z"
+        fill="white"
+      />
+      <circle cx="13" cy="11" r="3" stroke={color} strokeWidth="2" />
+      <path
+        d="M19 21C19 17.6863 16.3137 15 13 15C9.68629 15 7 17.6863 7 21"
+        stroke={color}
+        strokeWidth="2"
+      />
+    </svg>
+  );
 };
 
 export default IconRecomend;
